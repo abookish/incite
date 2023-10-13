39a2c8e430c782c458a566dbf4faf83d5a497935
stephanotice <snsteinca@gmail.com>
2023-10-12 19:27:09 -0700

    placeholder tasks, title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import {TaskListItem} from './Task'
 import './App.css';
+import Title from './beautifulTitle';
 
-function App() {
-  return  <>Hello World</>;
+//need task prompt
+const tasks: Task[] = [
+  {
+    text: 'Make nice little sections',
+    complete: false,
+    value: 4,
+    category: 'first'
+  },
+  {
+    text: 'Go through little tutorial',
+    complete: true,
+    value: 2,
+    category: 'second'
+  },
+];
+
+function App() { //passing in tasks
+  return  <> 
+   <Title title='TODO' subtitle = 'submerge  in the task'/>
+  <TaskListItem task = {tasks[0]}/> 
+  <TaskListItem task = {tasks[1]}/> 
+  </>;
 }
 
 export default App;
