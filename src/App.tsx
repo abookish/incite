919083a2cd8f2a24403ad97470e663d4209cd94a
stephanotice <snsteinca@gmail.com>
2023-10-13 20:36:15 -0700

    basically workingggg

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import {TaskListItem} from './Task'
 import './App.css';
 import Title from './beautifulTitle';
+import { TaskList } from './TaskList';
+import { AddTaskForm } from './addTaskForm';
 
-//need task prompt
-const tasks: Task[] = [
+//todo get rid of
+const demoTasks: Task[] = [
   {
     text: 'Make nice little sections',
     complete: false,
     value: 4,
-    category: 'first'
+    category: null
   },
   {
     text: 'Go through little tutorial',
     complete: true,
     value: 2,
-    category: 'second'
+    category: null
   },
 ];
 
-function App() { //passing in tasks
+function App() { 
+  
+  const [tasks, setTodos] = useState(demoTasks);
+//toggle function todo abstract
+  const toggleTodo = (selectedTodo: Task) => {
+    const newTasks = tasks.map((task) => {
+      if (task === selectedTodo) {
+        return {
+          ...task,
+          complete: !task.complete,
+        };
+      }
+      return task;
+    });
+    setTodos(newTasks);
+  };
+  
+  const addTask: AddTask = (text: string) => {
+    const newTask = { text, complete: false, category: null};
+    setTodos([...tasks, newTask]);
+  };
   return  <> 
    <Title title='TODO' subtitle = 'submerge  in the task'/>
-  <TaskListItem task = {tasks[0]}/> 
-  <TaskListItem task = {tasks[1]}/> 
-  </>;
+    <TaskList tasks={tasks} toggleTodo={toggleTodo} />
+    <AddTaskForm addTask={addTask}/>
+  </>
 }
 
 export default App;
